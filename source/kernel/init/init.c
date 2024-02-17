dfe79b8a9ad248fb34bd3f681b7dfd606a1bb9a5
Your Name <you@example.com>
2024-02-17 19:00:21 +0800

    Added kernel moudle and jmp to kernel_main
    Signed-off-by: Your Name <you@example.com>

---
+++ source/kernel/init/init.c
@@ -0,0 +1,8 @@
+#include "comm/boot_info.h"
+
+/**
+ * 内核入口
+ */
+void kernel_init (boot_info_t * boot_info) {
+    for (;;) {}
+}
--- source/loader/loader_32.c
+++ source/loader/loader_32.c
@@ -32,6 +32,6 @@ static void read_disk(int sector,int sector_count,uint8_t *buf)
 }
 void load_kernel(void) {
     read_disk(100, 500, (uint8_t *)SYS_KERNEL_LOAD_ADDR);
-
+    ((void (*)(void))SYS_KERNEL_LOAD_ADDR)();
     for (;;) {}
 }
