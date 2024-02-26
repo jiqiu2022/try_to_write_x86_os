1f65df86c160003fac713a143b85390ee2bc5166
Your Name <you@example.com>
2024-02-26 19:14:25 +0800

    利用汇编模板处理异常错误函数 并使用结构体解析固定栈内容
    Signed-off-by: Your Name <you@example.com>

--- source/kernel/cpu/irq.c
+++ source/kernel/cpu/irq.c
@@ -6,7 +6,19 @@
 static gate_desc_t idt_table[IDT_TABLE_NR];	// 中断描述表
 void irq_init(void) {	
 	for (uint32_t i = 0; i < IDT_TABLE_NR; i++) {
-        gate_desc_set(idt_table + i, 0, 0, 0);
+    	gate_desc_set(idt_table + i, KERNEL_SELECTOR_CS, (uint32_t) exception_handler_unknown,
+                  GATE_P_PRESENT | GATE_DPL0 | GATE_TYPE_IDT);
 	}
 	lidt((uint32_t)idt_table, sizeof(idt_table));
+}
+static void do_default_handler (exception_frame_t* frame, char * message) {
+    for (;;) {}
+}
+
+void do_handler_unknown (exception_frame_t * frame) {
+	do_default_handler(frame, "Unknown exception.");
+}
+
+void do_handler_divider(exception_frame_t * frame) {
+	do_default_handler(frame, "Device Error.");
 }
--- source/kernel/init/init.c
+++ source/kernel/init/init.c
@@ -1,11 +1,13 @@
 #include "comm/boot_info.h"
 #include "cpu/cpu.h"
+#include "cpu/irq.h"
 /**
  * 内核入口
  */
 static boot_info_t * init_boot_info; 
 void kernel_init (boot_info_t * boot_info) {
     cpu_init();
+    irq_init();
     init_boot_info =boot_info;
 }
 void init_main(void) {
